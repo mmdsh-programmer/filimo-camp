a15eaff7398f45dc85afe684859dae0946d165df
mmdsh-programmer <programmer.mmdsh@gmail.com>
2022-02-15 16:47:34 +0330

    add tailwind css to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import React from 'react'
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
